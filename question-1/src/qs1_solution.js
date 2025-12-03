cd576327c5dada318b45f5ac01f3143b34fbe938
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-12-03 10:25:11 +0530

    solved part 2

--- question-1/src/qs1_solution.js
+++ question-1/src/qs1_solution.js
@@ -1,21 +1,34 @@
-export const calculateRequiredFuel = (mass) => {
-  return Math.floor(mass / 3) - 2;
+export const calculateRequiredFuel = (mass) => Math.floor(mass / 3) - 2;
+
+export const calculateFuel = mass => {
+  const fuel = calculateRequiredFuel(mass);
+  if(fuel <= 0) {
+    return 0;
+  }
+  return fuel + calculateFuel(fuel);
 };
 
 const parseInput = (input) =>
   input.split("\n").map((module) => parseInt(module));
 
 export const part1Solution = (input) => {
-  const modules = parseInput(input);
+  const masses = parseInput(input);
   const requiredFuelAmounts = [];
 
-  for (const module of modules) {
-    requiredFuelAmounts.push(calculateRequiredFuel(module));
+  for (const mass of masses) {
+    requiredFuelAmounts.push(calculateRequiredFuel(mass));
   }
 
   return requiredFuelAmounts.reduce((sum, fuel) => sum + fuel, 0);
 };
 
 export const part2Solution = input => {
-  
+  const masses = parseInput(input);
+  const requiredFuelAmounts = [];
+
+  for (const mass of masses) {
+    requiredFuelAmounts.push(calculateFuel(mass));
+  }
+
+  return requiredFuelAmounts.reduce((sum, fuel) => sum + fuel, 0);
 };
--- question-1/test/qs1_solution_test.js
+++ question-1/test/qs1_solution_test.js
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { calculateRequiredFuel, part1Solution } from "../src/qs1_solution.js";
+import { calculateFuel, calculateRequiredFuel, part1Solution, part2Solution } from "../src/qs1_solution.js";
 
 Deno.test("simple test", () => {
   assertEquals(calculateRequiredFuel(12), 2);
@@ -29,7 +29,24 @@ Deno.test("simple test for multi modules", () => {
   assertEquals(part1Solution("12\n14\n1969\n100756"), 34241);
 });
 
-Deno.test("large input", () => {
+Deno.test("part 1 : large input", () => {
   const input = Deno.readTextFileSync("./data/input.txt");
   assertEquals(part1Solution(input), 3297626);
+});
+
+Deno.test("part 2 : simple input", () => {
+  assertEquals(calculateFuel(12), 2);
+});
+
+Deno.test("part 2 : simple test for one module", () => {
+  assertEquals(part2Solution("12"), 2);
+});
+
+Deno.test("part 2 : simple test for two modules", () => {
+  assertEquals(part2Solution("12\n14"), 4);
+});
+
+Deno.test("part 2 : large input", () => {
+  const input = Deno.readTextFileSync("./data/input.txt");
+  assertEquals(part2Solution(input), 4943578);
 });
