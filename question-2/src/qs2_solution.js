d1257d96762cf727b93ee2c0cf7835e993b4bfcb
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-12-03 13:59:22 +0530

    solved part 2

--- question-2/src/qs2_solution.js
+++ question-2/src/qs2_solution.js
@@ -15,10 +15,9 @@ const executeInstruction = (opcodes, index) => {
   );
 };
 
-export const sprint1 = (input) => {
-  const opcodes = parse(input);
-  opcodes[1] = 12;
-  opcodes[2] = 2;
+export const sprint1 = (opcodes, noun, verb) => {
+  opcodes[1] = noun;
+  opcodes[2] = verb;
   let index = 0;
 
   while (index < opcodes.length - 1 && opcodes[index] !== 99) {
@@ -26,5 +25,21 @@ export const sprint1 = (input) => {
     index += 4;
   }
 
-  return opcodes.join(",");
+  return opcodes[0];
+};
+
+export const sprint2 = (input, output) => {
+  let opcodes = parse(input);
+  const orginialInput = [...opcodes];
+
+  for(let i = 0; i < 100; i++) {
+    for(let j = 0; j < 100; j++) {
+      const result = sprint1(opcodes, i, j);
+
+      if(result === output) {
+        return [i, j];
+      }
+      opcodes = [...orginialInput];
+    }
+  }
 };
