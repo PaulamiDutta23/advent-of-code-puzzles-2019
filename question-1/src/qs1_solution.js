4b986b9a983805ef6eb53920c541c4bff2cd656e
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-12-03 08:08:34 +0530

    added simple testcase in qs 1

---
+++ question-1/src/qs1_solution.js
@@ -0,0 +1,3 @@
+export const part1Solution = (mass) => {
+  
+};
---
+++ question-1/test/qs1_solution_test.js
@@ -0,0 +1,4 @@
+import { assertEquals } from "@std/assert";
+import { part1Solution } from "../src/qs1_solution.js";
+
+Deno.test("simple test", () => {assertEquals(part1Solution(12), 2)});
