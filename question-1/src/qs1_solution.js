a537c7722036578a3aea879b90d59e0c91f02119
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-12-03 08:23:47 +0530

    modified functions

--- question-1/src/qs1_solution.js
+++ question-1/src/qs1_solution.js
@@ -1,3 +1,13 @@
 export const calculateRequiredFuel = (mass) => {
   return Math.floor(mass / 3) - 2;
 };
+
+export const part1Solution = (input) => {
+  const requiredFuelAmounts = [];
+  
+  for (const module of input) {
+    requiredFuelAmounts.push(calculateRequiredFuel(module));
+  }
+
+  return requiredFuelAmounts.reduce((sum, fuel) => sum + fuel, 0);
+};
--- question-1/test/qs1_solution_test.js
+++ question-1/test/qs1_solution_test.js
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { calculateRequiredFuel } from "../src/qs1_solution.js";
+import { calculateRequiredFuel, part1Solution } from "../src/qs1_solution.js";
 
 Deno.test("simple test", () => {
   assertEquals(calculateRequiredFuel(12), 2);
@@ -15,4 +15,16 @@ Deno.test("module with mass 1969", () => {
 
 Deno.test("module with mass 100756", () => {
   assertEquals(calculateRequiredFuel(100756), 33583);
+});
+
+Deno.test("simple test for two modules", () => {
+  assertEquals(part1Solution([12, 14]), 4);
+});
+
+Deno.test("simple test for one module", () => {
+  assertEquals(part1Solution([12]), 2);
+});
+
+Deno.test("simple test for multi modules", () => {
+  assertEquals(part1Solution([12, 14, 1969, 100756]), 34241);
 });
