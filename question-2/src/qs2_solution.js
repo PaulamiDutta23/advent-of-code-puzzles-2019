44b939b09ecaeb60a2f6fd307ce46e33905f2d2e
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-12-14 21:23:39 +0530

    modified intcode progra from question-2

--- question-2/src/qs2_solution.js
+++ question-2/src/qs2_solution.js
@@ -1,43 +1,97 @@
+const givenInput = Deno.readTextFileSync("./question-2/data/input.txt");
+
+const INPUTS = {
+  simpleAdd: { program: "1,0,0,0,99", overrider: [] },
+  simpleMul: { program: "2,3,0,3,99", overrider: [] },
+  simpleSquare: { program: "2,4,4,5,99,0", overrider: [] },
+  simpleModificaion: { program: "1,1,1,4,99,5,6,0,99", overrider: [] },
+  puzzleInput: { program: givenInput, overrider: [[1, 12], [2, 2]] },
+};
+
+const input = INPUTS.puzzleInput;
+
 export const parse = (input) =>
   input.split(",").map((value) => parseInt(value));
 
-const add = (a, b) => a + b;
-const mul = (a, b) => a * b;
-
-const opcodeDetails = {
-  1: add,
-  2: mul,
+const createComputer = (program) => {
+  return {
+    currentPosition: 0,
+    program,
+    isHalted: false,
+  };
 };
 
-const executeInstruction = (opcodes, index) => {
-  opcodes[opcodes[index + 3]] = opcodeDetails[opcodes[index]](
-    opcodes[opcodes[index + 1]],
-    opcodes[opcodes[index + 2]],
-  );
-};
-
-export const sprint1 = (opcodes, noun, verb) => {
-  opcodes[1] = noun;
-  opcodes[2] = verb;
-  let index = 0;
-
-  while (index < opcodes.length - 1 && opcodes[index] !== 99) {
-    executeInstruction(opcodes, index);
-    index += 4;
+const override = (computer, values = []) => {
+  for (const value of values) {
+    const [position, overrideWith] = value;
+    computer.program[position] = overrideWith;
   }
-
-  return opcodes[0];
 };
+
+const determineAddresses = (program, position) => {
+  return {
+    input1Loc: program[position + 1],
+    input2Loc: program[position + 2],
+    outputLoc: program[position + 3],
+  };
+};
+
+const add = (program, { input1Loc, input2Loc, outputLoc }) => {
+  program[outputLoc] = program[input1Loc] + program[input2Loc];
+};
+
+const mul = (program, { input1Loc, input2Loc, outputLoc }) => {
+  program[outputLoc] = program[input1Loc] * program[input2Loc];
+};
+
+const halt = (_program, _locations, computer) => {
+  computer.isHalted = true;
+};
+
+const OPCODES = {
+  1: { operation: add, stepsToMove: 4 },
+  2: { operation: mul, stepsToMove: 4 },
+  99: { operation: halt, stepsToMove: 0 },
+};
+
+const stepForward = (computer) => {
+  const curProgram = computer.program;
+  const currentPosition = computer.currentPosition;
+  const opcode = curProgram[currentPosition];
+  const locations = determineAddresses(
+    curProgram,
+    currentPosition,
+  );
+  OPCODES[opcode].operation(curProgram, locations, computer);
+  computer.currentPosition += OPCODES[opcode].stepsToMove;
+  return computer;
+};
+
+const execute = (computer) => {
+  while (!computer.isHalted) {
+    stepForward(computer);
+  }
+};
+
+export const sprint1 = (input) => {
+  const modifiedProgram = parse(input.program);
+  const computer = createComputer(modifiedProgram);
+  override(computer, input.overrider);
+  execute(computer);
+  return computer;
+};
+
+console.log(sprint1(input));
 
 export const sprint2 = (input, output) => {
   let opcodes = parse(input);
   const orginialInput = [...opcodes];
 
-  for(let i = 0; i < 100; i++) {
-    for(let j = 0; j < 100; j++) {
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
       const result = sprint1(opcodes, i, j);
 
-      if(result === output) {
+      if (result === output) {
         return [i, j];
       }
       opcodes = [...orginialInput];
